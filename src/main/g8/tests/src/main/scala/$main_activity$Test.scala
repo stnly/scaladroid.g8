e5a20c5183d04bae56c3681456e66d2b0f327e9f
Stanley Tan <me@stnly.com>
2013-04-13 14:40:05 +1000

    Add tests module

---
+++ src/main/g8/tests/src/main/scala/$main_activity$Test.scala
@@ -0,0 +1,20 @@
+package $package$.tests
+
+import $package$._
+import junit.framework.Assert._
+import _root_.android.test.AndroidTestCase
+import _root_.android.test.ActivityInstrumentationTestCase2
+
+class AndroidTests extends AndroidTestCase {
+  def testPackageIsCorrect() {
+    assertEquals("$package$", getContext.getPackageName)
+  }
+}
+
+class ActivityTests extends ActivityInstrumentationTestCase2(classOf[$main_activity$]) {
+   def testHelloWorldIsShown() {
+      val activity = getActivity
+      val textview = activity.findView(TR.textview)
+      assertEquals(textview.getText, "Hello World! - Scaladroid")
+    }
+}
